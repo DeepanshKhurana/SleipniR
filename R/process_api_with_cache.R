ccbd344fd51b3edfa56ba2ef6b7a9bf342c7ecd5
deepanshkhurana <deepanshkhurana@outlook.com>
2024-10-06 10:47:45 +0530

    fix: add correct cached_status if the API is processed fresh

--- R/process_api_with_cache.R
+++ R/process_api_with_cache.R
@@ -3,14 +3,15 @@
 #' @return A list with the response and the status of the cache.
 #' @export
 process_api_with_cache <- function(endpoint) {
-  cached_status <- get_cache_status(endpoint)
-  if (cached_status$cron_status %in% c("yellow", "green")) {
+  cached_status <- get_cache_status(endpoint)$cron_status
+  if (cached_status %in% c("yellow", "green")) {
     response <- process_api(endpoint, cached = TRUE)
   } else {
+    cached_status <- "green"
     response <- process_api(endpoint, cached = FALSE)
   }
   list(
     response = response,
-    status = cached_status$cron_status
+    status = cached_status
   )
 }
